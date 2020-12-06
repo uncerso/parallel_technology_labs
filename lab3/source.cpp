25ba9fb3ca582b5b707919e5ee1ffe806511bcef
uncerso <yayayayaya38@gmail.com>
2020-12-07 01:45:29 +0300

    'main' simplification

--- lab3/source.cpp
+++ lab3/source.cpp
@@ -1,79 +1,11 @@
-#include "matrixes.hpp"
+#include "helpers.hpp"
 #include <iostream>
 #include <random>
 #include <memory>
-#include <mpi.h>
 #include <exception>
 
 using namespace std;
 constexpr int dim_size = 2;
-
-struct InitGuard {
-    InitGuard(int argc, char **argv) {
-        MPI::Init(argc, argv);
-    }
-
-    ~InitGuard() {
-        MPI::Finalize();
-    }
-};
-
-struct ProcInfo {
-    int rank = 0;
-    size_t x = 0;
-    size_t y = 0;
-};
-
-struct Comms {
-    MPI::Cartcomm grid;
-    MPI::Cartcomm col;
-    MPI::Cartcomm row;
-};
-
-struct State {
-    RegularMatrix persistent_a;
-    RegularMatrix a, c;
-    unique_ptr<RegularMatrix> b1, b2;
-    State(size_t n) 
-        : persistent_a(n)
-        , a(n)
-        , c(n)
-        , b1(make_unique<RegularMatrix>(n))
-        , b2(make_unique<RegularMatrix>(n))
-    {}
-};
-
-class Win {
-    MPI::Win win;
-    MPI::Cartcomm const & comm;
-public:
-    Win(RegularMatrix const & mt, MPI::Cartcomm const & comm) 
-        : win(MPI::Win::Create(mt.data(), static_cast<MPI::Aint>(mt.size()), sizeof(double), MPI::INFO_NULL, comm))
-        , comm(comm)
-    {}
-
-    void Get(RegularMatrix & to, size_t target_rank) const {
-        auto data_size = static_cast<int>(to.size());
-        win.Get(to.data(), data_size, MPI_DOUBLE, static_cast<int>(target_rank), 0, data_size, MPI_DOUBLE);
-    }
-
-    void Put(RegularMatrix & from, size_t target_rank) const {
-        auto data_size = static_cast<int>(from.size());
-        win.Put(from.data(), data_size, MPI_DOUBLE, static_cast<int>(target_rank), 0, data_size, MPI_DOUBLE);
-    }
-
-    void Fence() const {
-        win.Fence(0);
-    }
-};
-
-struct WinVector : vector<Win> {
-    void FenceAll() const {
-        for (auto const & win : *this)
-            win.Fence();
-    }
-};
-
 
 // generate random matrix with elements from `-max_value` to `max_value`
 RegularMatrix GetRandomMatrix(size_t n, size_t max_value = 99) {
@@ -90,6 +22,8 @@ RegularMatrix GetRandomMatrix(size_t n, size_t max_value = 99) {
 
 Comms CreateGridCommunicators(size_t grid_size) {
     Comms comms;
+    comms.grid_size = grid_size;
+
     int dims[dim_size] = {static_cast<int>(grid_size), static_cast<int>(grid_size)};
     bool periodic[dim_size] = {false, false};
     
@@ -121,82 +55,102 @@ pair<size_t, size_t> GetCoords(MPI::Cartcomm const & cc, int rank) {
     return {coords[0], coords[1]};
 }
 
-int main(int argc, char *argv[]) {
-    InitGuard mpi_guard(argc, argv);
-    auto proc_num = static_cast<size_t>(MPI::COMM_WORLD.Get_size());
-    ProcInfo proc_info;
-    proc_info.rank = MPI::COMM_WORLD.Get_rank();
+struct BlockMatrixPack {
+    unique_ptr<BlockMatrix> a, b, c;
+};
 
-    auto grid_size = static_cast<size_t>(sqrt(static_cast<double>(proc_num)));
-    if (proc_num != grid_size*grid_size) {
-        if (proc_info.rank == 0)
-            cout << "Number of processes must be a perfect square\n";
-        return 0;
+BlockMatrixPack GenMatrixPack(bool dummied, size_t block_size, size_t grid_size) {
+    BlockMatrixPack pack;
+    if (!dummied) {
+        size_t matrix_size = block_size * grid_size;
+        pack.a = make_unique<BlockMatrix>(GetRandomMatrix(matrix_size), block_size);
+        pack.b = make_unique<BlockMatrix>(GetRandomMatrix(matrix_size), block_size);
+        pack.c = make_unique<BlockMatrix>(grid_size, block_size);
+    } else {
+        pack.a = make_unique<BlockMatrix>(grid_size, block_size);
+        pack.b = make_unique<BlockMatrix>(grid_size, block_size);
+        pack.c = make_unique<BlockMatrix>(grid_size, block_size);
     }
 
-    if (proc_info.rank == 0)
-        cout << "Parallel matrix multiplication program" << endl;
+    return pack;
+}
 
-    Comms comms = CreateGridCommunicators(grid_size);
-    tie(proc_info.y, proc_info.x) = GetCoords(comms.grid, proc_info.rank);
+void LoadData(State & st, BlockMatrixPack & pack, Comms const & comms, ProcInfo const & proc_info) {
+    auto ba_refs = GetRefs(*pack.a, comms.grid);
+    auto bb_refs = GetRefs(*pack.b, comms.grid);
 
-    size_t matrix_size = grid_size*15;
-    size_t block_size = matrix_size / grid_size;
+    auto lin_address = proc_info.y * comms.grid_size + proc_info.x;
+    ba_refs[lin_address].Get(st.a, 0);
+    bb_refs[lin_address].Get(*st.b1, 0);
+    ba_refs.FenceAll();
+    bb_refs.FenceAll();
+}
+
+void StoreData(State & st, BlockMatrixPack & pack, Comms const & comms, ProcInfo const & proc_info) {
+    auto lin_address = proc_info.y * comms.grid_size + proc_info.x;
+    auto bc_refs = GetRefs(*pack.c, comms.grid);
+    bc_refs[lin_address].Put(st.c, 0);
+    bc_refs.FenceAll();
+}
+
+void Multiply(State & st, Comms const & comms, ProcInfo const & proc_info) {
+    size_t grid_size = comms.grid_size;
+    st.persistent_a = st.a;
+    Win a_win(st.persistent_a, comms.row);
+    auto b1_win = make_unique<Win>(*st.b1, comms.col);
+    auto b2_win = make_unique<Win>(*st.b2, comms.col);
+    a_win.Fence();
+    b1_win->Fence();
+    b2_win->Fence();
+
+    for (size_t i = 0; i < grid_size; ++i) {
+        a_win.Get(st.a, (proc_info.y + i) % grid_size);
+        a_win.Fence();
+
+        st.c.AddProductOf(st.a, *st.b1);
+
+        b1_win->Get(*st.b2, (proc_info.y + 1) % grid_size);
+        b1_win->Fence();
+        swap(st.b1, st.b2);
+        swap(b1_win, b2_win);
+        comms.col.Barrier();
+    }
+}
+
+int main(int argc, char *argv[]) {
+    InitGuard mpi_guard(argc, argv);
     try {
-        unique_ptr<RegularMatrix> c;
-        unique_ptr<BlockMatrix> ba, bb, bc;
-        if (proc_info.rank == 0) {
-            auto a = make_unique<RegularMatrix>(GetRandomMatrix(matrix_size));
-            auto b = make_unique<RegularMatrix>(GetRandomMatrix(matrix_size));
-            c = make_unique<RegularMatrix>(matrix_size);
-            ba = make_unique<BlockMatrix>(*a, block_size);
-            bb = make_unique<BlockMatrix>(*b, block_size);
-            bc = make_unique<BlockMatrix>(grid_size, block_size);
-            c->AddProductOf(*a, *b);
-        } else {
-            ba = make_unique<BlockMatrix>(grid_size, block_size);
-            bb = make_unique<BlockMatrix>(grid_size, block_size);
-            bc = make_unique<BlockMatrix>(grid_size, block_size);
+        ProcInfo proc_info;
+        auto proc_num = static_cast<size_t>(MPI::COMM_WORLD.Get_size());
+        proc_info.rank = MPI::COMM_WORLD.Get_rank();
+
+        auto grid_size = static_cast<size_t>(sqrt(static_cast<double>(proc_num)));
+        if (proc_num != grid_size*grid_size) {
+            if (proc_info.rank == 0)
+                cout << "Number of processes must be a perfect square\n";
+            return 0;
         }
-        auto ba_refs = GetRefs(*ba, comms.grid);
-        auto bb_refs = GetRefs(*bb, comms.grid);
-        auto bc_refs = GetRefs(*bc, comms.grid);
+
+        if (proc_info.rank == 0)
+            cout << "Parallel matrix multiplication program" << endl;
+
+        Comms comms = CreateGridCommunicators(grid_size);
+        tie(proc_info.y, proc_info.x) = GetCoords(comms.grid, proc_info.rank);
+
+        size_t matrix_size = grid_size*15;
+        size_t block_size = matrix_size / grid_size;
 
         State st(block_size);
+        auto pack = GenMatrixPack(proc_info.rank, block_size, grid_size);
 
-        auto lin_address = proc_info.y * grid_size + proc_info.x;
-        ba_refs[lin_address].Get(st.a, 0);
-        bb_refs[lin_address].Get(*st.b1, 0);
-        ba_refs.FenceAll();
-        bb_refs.FenceAll();
+        LoadData(st, pack, comms, proc_info);
 
-        st.persistent_a = st.a;
-        Win a_win(st.persistent_a, comms.row);
-        auto b1_win = make_unique<Win>(*st.b1, comms.col);
-        auto b2_win = make_unique<Win>(*st.b2, comms.col);
-        a_win.Fence();
-        b1_win->Fence();
-        b2_win->Fence();
-        for (size_t i = 0; i < grid_size; ++i) {
-            a_win.Get(st.a, (proc_info.y + i) % grid_size);
-            a_win.Fence();
+        Multiply(st, comms, proc_info);
 
-            st.c.AddProductOf(st.a, *st.b1);
+        StoreData(st, pack, comms, proc_info);
 
-            b1_win->Get(*st.b2, (proc_info.y + 1) % grid_size);
-            b1_win->Fence();
-            swap(st.b1, st.b2);
-            swap(b1_win, b2_win);
-            comms.col.Barrier();
-        }
-
-        bc_refs[lin_address].Put(st.c, 0);
-        bc_refs.FenceAll();
-
-        if (proc_info.rank == 0) {
-            BlockMatrix bm(*c, block_size);
-            cout << (bm == *bc) << endl;
-        }
+        if (proc_info.rank == 0)
+            cout << "Done" << endl;
     } catch (exception const & e) {
         cerr << e.what() << '\n';
         return 0;
